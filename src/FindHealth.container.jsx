b0af735bc304a76295d9bdd84c8eaaec2c2e395b
saumya <2saumyaray@gmail.com>
2021-11-14 17:05:56 +0530

    initial app

---
+++ src/FindHealth.container.jsx
@@ -0,0 +1,79 @@
+//
+const FindHealthApp = ()=>{
+    return(
+        <div>
+            <header>
+                <h1> Find Health </h1>
+                <p> You will takecare of it. May be.</p>
+            </header>
+            <main>
+                <section>
+                    <h2>
+                        Service, shutdown.
+                    </h2>
+                    <p>
+                        The service project is now down. 
+                        We <mark>do not support</mark> it anymore in terms of <mark>hand holding</mark>.
+                        However, the project is always available to anyone who wants to use it.
+                    </p>
+                </section>
+                <section>
+                   
+                    <details>
+                        <summary> Why? </summary>
+                        <p>
+                            Mostly support. To run it we needed funding and could not run it after sometime.
+                            Initially, we thought the project can survive with donations. It did not happen.
+                            <ol>
+                                <li> The adoption rate was too slow. </li>
+                                <li> 
+                                    No one seems to understand privacy! 
+                                    It became too much to push that idea now. Probably, it will happen in future. 
+                                </li>
+                            </ol>
+                        </p>
+                    </details>
+                    <details>
+                        <summary> Code! </summary>
+                        <p>
+                            <mark>Code is always free and opensource.</mark>
+                            From the beginning it is our idea to work in open.
+                            It stays open.
+                            <ol>
+                                <li> Free </li>
+                                <li> Open </li>
+                                <li> No Corporate </li>
+                                <li> Privacy first </li>
+                            </ol>
+                        </p>
+                    </details>
+
+                </section>
+
+            </main>
+            <footer>
+                <h1> Thank you. </h1>
+                <p>
+                    It was a nice run. We made it for the purpose of Health and Privacy. 
+                    It is achievable and very much a possibilty. 
+                    Currently <i>people are obsessed with medicine than health</i>. 
+                    It is about the time, when people will care about their own health.
+                </p>
+                <p>
+                    Privacy is another thing to make people understand today. This is sure will comeback soon.
+                </p>
+                <section>
+                    <p> This site is made with </p>
+                    <nav>
+                        <h4> Made with </h4>
+                        <a href="https://vitejs.dev/"> Vite </a>
+                        <a href="https://reactjs.org/"> React </a>
+                        <a href="https://simplecss.org/"> Simple CSS </a>
+                    </nav>
+                </section>
+                
+            </footer>
+        </div>
+    )
+}
+export default FindHealthApp
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import FindHealthApp from './FindHealth.container'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FindHealthApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
